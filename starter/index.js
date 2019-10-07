a74efff90a88c7fa40f1a114058e0a990fd4d88a
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2019-10-07 20:19:55 +0300

    Add files via upload

---
+++ starter/index.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+
+///// Files ///////////////////////////////////////////////////////
+
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+
+//         console.log("this first dadt2: " + data2);
+//         fs.readFile('./txt/append4.txt', 'utf-8', (err, data3) => {
+
+//             if (err) {
+//                 return console.log('ERROR! 🤯🤢');
+//             } else {
+
+//                 console.log("data3 " + data3);
+//             }
+
+//             fs.writeFile('./txt/final.txt', `data2: ${data2} \n data3: ${data3}`, 'utf-8', err => {
+//                 if (err) {
+//                     return console.log('ERROR! 🤯🤢');
+//                 } else {
+
+//                     console.log("finished writing 😆");
+//                 }
+
+//             })
+//         });
+//     });
+// });
+// console.log("sync ");
+
+// const textOut = `This is what we know about the Avocado: ${textIn}.\nCreated on ${Date(Date.now()).toString()}`;
+
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File was written!');
+// const readTextOut = fs.readFileSync('./txt/output.txt', 'utf-8');
+// console.log(readTextOut);
+
+///// Server ///////////////////////////////////////////////////////
+const replaceTemplate = (temp, product) => {
+
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
+
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
+
+const server = http.createServer((req, res) => {
+
+    const pathName = req.url;
+
+    // Overview Page////////////////////////////
+
+    if (pathName === '/' || pathName === '/overview') {
+
+        res.writeHead(200, { 'Content-type': 'text/html' });
+
+        const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        console.log(cardsHtml);
+        res.end(output);
+
+        // Product Page /////////////////////////////
+    } else if (pathName === '/product') {
+
+        res.end('This is the Product page');
+
+        // API /////////////////////////////
+    } else if (pathName === '/api') {
+        console.log('in the api');
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+
+        // Not Found Page /////////////////////////////
+    } else {
+
+        res.writeHead(404, {
+
+            'content-type': 'text/html',
+            'my-own-header': 'hello-world'
+
+        });
+        res.end('<h1 style="color:purple;">You asked the WRONG page ASSHOLE!</h1>\n<img src="https://images.unsplash.com/photo-1521996319423-90475f382dff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60">');
+    }
+
+});
+
+server.listen(8000, '127.0.0.1', () => console.log('Lisetning to requests on port 8000'));
+
